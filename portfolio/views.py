99916048fb92ec1636b9ff0cfb7372b27d4bb1c0
Oluwafemi Ayeyemi <femiayeyemi@live.com>
2026-03-12 20:40:35 +0100

    adding blog

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
-from .models import Project, Testimonial
+from blog.models import Post  # Import the new database model
 
 
-def home(request):
-    # Fetch all projects, ordered by the newest first (based on model Meta)
-    projects = Project.objects.all()
-    # Fetch only active testimonials
-    testimonials = Testimonial.objects.filter(is_active=True)
+def index(request):
+    # Fetch the 3 most recent published posts
+    recent_posts = Post.objects.filter(
+        is_published=True).order_by('-published_at')[:3]
 
+    # Pass them to the template
     context = {
-        'projects': projects,
-        'testimonials': testimonials,
+        'recent_posts': recent_posts
     }
     return render(request, 'portfolio/index.html', context)
