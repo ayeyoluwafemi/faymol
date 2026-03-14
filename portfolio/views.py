d9e0356b5ad9e1bbc73293a11733e84f15a3940d
Oluwafemi Ayeyemi <femiayeyemi@live.com>
2026-03-14 22:46:10 +0100

    updated the portfolio listing

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
-from blog.models import Post  # Import the new database model
+# Import the Project model from the current (portfolio) app
+from .models import Project
+from blog.models import Post  # Your existing blog import
+
+# If you also created a Testimonial model, you can import it like this:
+# from .models import Project, Testimonial
 
 
 def index(request):
-    # Fetch the 3 most recent published posts
+    # 1. Fetch the projects
+    # We are grabbing all projects here. If you added an 'is_published' or 'is_featured'
+    # field to your Project model, you can filter it just like you did with posts!
+    featured_projects = Project.objects.all()
+
+    # 2. Fetch the 3 most recent published posts
     recent_posts = Post.objects.filter(
         is_published=True).order_by('-published_at')[:3]
 
-    # Pass them to the template
+    # 3. Fetch testimonials (Uncomment if you have this model set up in your database)
+    # testimonials = Testimonial.objects.all()
+
+    # Pass everything to the template
     context = {
-        'recent_posts': recent_posts
+        'featured_projects': featured_projects,
+        'recent_posts': recent_posts,
+        # 'testimonials': testimonials,
     }
+
     return render(request, 'portfolio/index.html', context)
